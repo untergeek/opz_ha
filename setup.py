a8dc3db2dd5523c395cd5d83e4713acf366c8669
Aaron Mildenstein <aaron@mildensteins.com>
2018-02-16 11:15:24 -0700

    Release prep building

---
+++ setup.py
@@ -0,0 +1,86 @@
+import os
+import re
+import sys
+from setuptools import setup
+
+# Utility function to read from file.
+def fread(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def get_version():
+    VERSIONFILE="opz_ha/_version.py"
+    verstrline = fread(VERSIONFILE).strip()
+    vsre = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    mo = re.search(vsre, verstrline, re.M)
+    if mo:
+        VERSION = mo.group(1)
+    else:
+        raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+    build_number = os.environ.get('OPZ_HA_BUILD_NUMBER', None)
+    if build_number:
+        return VERSION + "b{}".format(build_number)
+    return VERSION
+
+def get_install_requires():
+    return [
+        'pyA20',
+        'paho-mqtt'
+        'click>=6.7',
+        'pyyaml>=3.10',
+    ]
+
+try:
+    ### cx_Freeze ###
+    from cx_Freeze import setup, Executable
+    # Dependencies are automatically detected, but it might need
+    # fine tuning.
+
+    base = 'Console'
+
+    icon = None
+    opz_ha_exe = Executable(
+        "opz_ha.py",
+        base=base,
+        targetName = "opz_ha_relay",
+    )
+    buildOptions = dict(
+        packages = [],
+        excludes = [],
+        include_files = [],
+    )
+
+    setup(
+        name = "opz_ha",
+        version = get_version(),
+        author = "Aaron Mildenstein",
+        description = "Tiny remote MQTT relay of GPIO data from Orange Pi Zero single-board computers",
+        long_description=fread('README.rst'),
+        url = "http://github.com/untergeek/opz_ha",
+        download_url = "https://github.com/untergeek/opz_ha/tarball/v" + get_version(),
+        license = "Apache License, Version 2.0",
+        install_requires = get_install_requires(),
+        keywords = "orangepi mqtt home automation",
+        packages = ["opz_ha"],
+        include_package_data=True,
+        entry_points = {
+            "console_scripts" : [
+                "opz_ha_relay = opz_ha.cli:cli",
+            ]
+        },
+        classifiers=[
+            "Topic :: Home Automation",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: Apache Software License",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+        ],
+#        test_suite = "test.run_tests.run_all",
+#        tests_require = ["mock", "nose", "coverage", "nosexcover"],
+        options = {"build_exe" : buildOptions},
+        executables = [opz_ha_exe]
+    )
+    ### end cx_Freeze ###
+except ImportError:
+    setup(
+        test_suite = "test.run_tests.run_all",
+    )
