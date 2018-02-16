8c336d02ac689cf429a46cb94a026e1db4778b68
Aaron Mildenstein <aaron@mildensteins.com>
2018-02-16 09:33:15 -0700

    Oops.  No except.

--- opz_ha/cli.py
+++ opz_ha/cli.py
@@ -64,9 +64,8 @@ def cli(configuration_file, daemonize):
         logger.info('Daemonizing process...')
         with daemon.DaemonContext():
             mqttc, mqttGDO = run(config)
-            try:
-                while True:
-                    time.sleep(1)
+            while True:
+                time.sleep(1)
     else:
         logger.info('Running in foreground...')
         mqttc, mqttGDO = run(config)
