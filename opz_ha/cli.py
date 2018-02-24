3b1fde43bc177dce6d77e1f4c41c4bf5121dfae1
Aaron Mildenstein <aaron@mildensteins.com>
2018-02-23 17:09:18 -0700

    Add check_config to cli.py

--- opz_ha/cli.py
+++ opz_ha/cli.py
@@ -6,7 +6,8 @@ import paho.mqtt.client as mqtt
 import opz_ha.defaults as defaults
 from opz_ha.launcher import gdorelay, onewire, reedswitch
 from opz_ha._version import __version__
-from opz_ha.utils    import TerminationCatcher, cleanup_channels, process_config, rm_pid, write_pid
+from opz_ha.utils    import TerminationCatcher, check_config, cleanup_channels, 
+                            process_config, rm_pid, write_pid
 
 if not os.getegid() == 0:
     sys.exit('opz_ha must be run as root')
